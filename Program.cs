9e667087410237d5ab5e636f0d596b2cc1366500
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-11 09:50:45 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("welcome to c#");
+            //Console.ReadKey();
+            string firstName;
+            string lastName;
+            Console.WriteLine("Enter First Name");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Welcome Mr.\\ Ms.  " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+        
+    }
+}
