9855c942288f40b4de22375a4f22ee410cd09259
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-11 09:54:08 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -12,14 +12,32 @@ namespace ConsoleApp1
         {
             //Console.WriteLine("welcome to c#");
             //Console.ReadKey();
-            string firstName;
-            string lastName;
-            Console.WriteLine("Enter First Name");
-            firstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name");
-            lastName = Console.ReadLine();
-            Console.WriteLine("Welcome Mr.\\ Ms.  " + firstName + " " + lastName);
-            Console.ReadLine();
+            //string firstName;
+            //string lastName;
+            //Console.WriteLine("Enter First Name");
+            //firstName = Console.ReadLine();
+            //Console.WriteLine("Enter Last Name");
+            //lastName = Console.ReadLine();
+            //Console.WriteLine("Welcome Mr.\\ Ms.  " + firstName + " " + lastName);
+            //Console.ReadLine();
+            //exa-3
+            string name;
+            int age;
+            char grade;
+            DateTime doj;
+            double salary;
+            Console.WriteLine("Enter Name");
+            name= Console.ReadLine();
+            Console.WriteLine("Enter Age");
+            age=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Grade");
+            grade = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Date of Joining");
+            doj = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Salary");
+            salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Name:\t" +name+   "\n age:\t" +age+ "\n Grade: \t" +grade+ "\n Salary:\t" +salary+ "\n Date of Joining:\t" +doj);
+            Console.ReadKey();
         }
         
     }
